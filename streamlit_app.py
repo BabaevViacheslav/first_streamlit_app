c3064528b3cd49ec373c14c892956bfeb1ee5b9d
BabaevViacheslav <BabaevViacheslav@gmail.com>
2023-07-22 14:30:23 +0300

    Update streamlit_app.py
    Add four smoothies to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Special Fruit Smoothie 🥝🍇')
+
+streamlit.text('🍌🥭🍇 Banan, Rasberry & Grapple Smoothie')
+streamlit.text('🥝🥭🍇 Kiwi, Rasberry & Grapple Smoothie')
+streamlit.text('🥝🍇 Kiwi & Grapple Smoothie')
+streamlit.text('🥝🥭 Kiwi & Rasberry Smoothie')
