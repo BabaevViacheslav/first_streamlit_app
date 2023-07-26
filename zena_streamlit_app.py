64698facc3c2049f0e9e366ae0b70296a1f8ac31
BabaevViacheslav <BabaevViacheslav@gmail.com>
2023-07-26 15:59:42 +0300

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -10,12 +10,12 @@ streamlit.title('Zena Amazing Athleisure Catalog')
 
 
 #Add button to load a list of colours
-#if streamlit.button('Get colour load list'):
+if streamlit.button('Get colour load list'):
   my_con = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_con.cursor() as my_cur:
     my_cur.execute("select * from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
     my_data_rows = my_cur.fetchall()
-    streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows.row[0])
 
 #for row in my_data_rows:
   #print("Colour: ", row[0])
