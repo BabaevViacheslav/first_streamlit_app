a0360adc8ceb72eeb975919b032eea21f01981e3
BabaevViacheslav <BabaevViacheslav@gmail.com>
2023-07-26 15:51:04 +0300

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -6,6 +6,8 @@ from urllib.error import URLError
 
 
 streamlit.title('Zena Amazing Athleisure Catalog')
+streamlit.header('🍌🥭 Special Fruit Smoothie 🥝🍇')
+
 
 #Add button to load a list of colours
 if streamlit.button('Get colour load list'):
@@ -14,4 +16,8 @@ if streamlit.button('Get colour load list'):
     my_cur.execute("select COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
     my_data_rows = my_cur.fetchall()
     streamlit.dataframe(my_data_rows)
+
+
+streamlit.header('🍌🥭 Special Fruit Smoothie2 🥝🍇')
+
 
