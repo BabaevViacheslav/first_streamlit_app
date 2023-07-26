a8c0be3458df2722882067875c1f48b6a718f3b3
BabaevViacheslav <BabaevViacheslav@gmail.com>
2023-07-26 15:49:30 +0300

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -6,3 +6,12 @@ from urllib.error import URLError
 
 
 streamlit.title('Zena Amazing Athleisure Catalog')
+
+#Add button to load a list of colours
+if streamlit.button('Get colour load list'):
+  my_con = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  with my_con.cursor() as my_cur:
+    my_cur.execute("select COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
+    my_data_rows = my_cur.fetchall()
+    streamlit.dataframe(my_data_rows)
+
